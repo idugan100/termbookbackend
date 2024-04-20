6dd604b6f3daba74888f22b2953d4b0792c0bba1
idugan100 <isaacd4444@gmail.com>
2024-04-19 22:57:27 -0400

    adding db file and database connection function

--- server/server.go
+++ server/server.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+func Connect(path string) (*sql.DB, error) {
+	conn, err := sql.Open("sqlite", ("file:" + path))
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
 
 func main() {
-
+	dbConnection, err := Connect("./database.db")
+	if err != nil {
+		panic(err)
+	}
+	dbConnection.Ping()
 	fmt.Println("hello server")
 
 }
