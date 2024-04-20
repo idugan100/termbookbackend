ed6e720329646d88aee5839e02b14d8d99b805d3
idugan100 <isaacd4444@gmail.com>
2024-04-20 02:54:15 -0400

    refactoring handlers into new file

---
+++ server/handlers.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func newEntry(w http.ResponseWriter, r *http.Request) {
+	var e Entry
+	err := json.NewDecoder(r.Body).Decode(&e)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if e.Content == "" || e.UserEmail == "" || e.Time.IsZero() {
+		http.Error(w, "missing field", http.StatusBadRequest)
+		return
+	}
+	_, err = dbConnection.Exec("INSERT INTO Entries (userEmail, content, time) VALUES (?,?,?)", e.UserEmail, e.Content, e.Time)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getUserEntries(w http.ResponseWriter, r *http.Request) {
+	email := r.PathValue("userEmail")
+	rows, err := dbConnection.Query("SELECT * FROM Entries WHERE userEmail=?", email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var entriesList []Entry
+	var entry Entry
+	for rows.Next() {
+		err := rows.Scan(&entry.UserEmail, &entry.Content, &entry.Time)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		entriesList = append(entriesList, entry)
+	}
+	jsonResult, err := json.Marshal(entriesList)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonResult)
+}
+
+func timeCheck(w http.ResponseWriter, r *http.Request) {
+	cutOff := time.Now().Add(-24 * time.Hour)
+	email := r.PathValue("userEmail")
+	rows, err := dbConnection.Query("SELECT * FROM Entries WHERE userEmail=? AND time > ? ", email, cutOff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	jsonResult, err := json.Marshal(Completed{IsCompleted: rows.Next()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonResult)
+}
--- server/server.go
+++ server/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"net/http"
 	"time"
 
@@ -21,89 +20,12 @@ type Completed struct {
 
 var dbConnection *sql.DB
 
-func Connect(path string) (*sql.DB, error) {
-	conn, err := sql.Open("sqlite", ("file:" + path))
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}
-
-func newEntry(w http.ResponseWriter, r *http.Request) {
-	var e Entry
-	err := json.NewDecoder(r.Body).Decode(&e)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if e.Content == "" || e.UserEmail == "" || e.Time.IsZero() {
-		http.Error(w, "missing field", http.StatusBadRequest)
-		return
-	}
-	_, err = dbConnection.Exec("INSERT INTO Entries (userEmail, content, time) VALUES (?,?,?)", e.UserEmail, e.Content, e.Time)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
-}
-
-func getUserEntries(w http.ResponseWriter, r *http.Request) {
-	email := r.PathValue("userEmail")
-	rows, err := dbConnection.Query("SELECT * FROM Entries WHERE userEmail=?", email)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var entriesList []Entry
-	var entry Entry
-	for rows.Next() {
-		err := rows.Scan(&entry.UserEmail, &entry.Content, &entry.Time)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		entriesList = append(entriesList, entry)
-	}
-	jsonResult, err := json.Marshal(entriesList)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonResult)
-}
-
-func timeCheck(w http.ResponseWriter, r *http.Request) {
-	cutOff := time.Now().Add(-24 * time.Hour)
-	email := r.PathValue("userEmail")
-	rows, err := dbConnection.Query("SELECT * FROM Entries WHERE userEmail=? AND time > ? ", email, cutOff)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	jsonResult, err := json.Marshal(Completed{IsCompleted: rows.Next()})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(jsonResult)
-}
-
 func main() {
 	var err error
 	dbConnection, err = Connect("./database.db")
 	if err != nil {
 		panic(err)
 	}
-	dbConnection.Ping()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /newentry", newEntry)
@@ -116,4 +38,12 @@ func main() {
 	}
 
 	panic(s.ListenAndServe())
+}
+
+func Connect(path string) (*sql.DB, error) {
+	conn, err := sql.Open("sqlite", ("file:" + path))
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
